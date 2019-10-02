3f7858ed67d993904c14831d653c0bf75e0f0f11
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-10-02 16:01:06 +0200

    upd

--- 1_kubeadm/reset.sh
+++ 1_kubeadm/reset.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-# Reset k8s cluster
+# Reset a k8s cluster an all nodes
 
 set -e
 
-sudo -- kubeadm reset -f
-sudo -- sh -c "iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X"
-sudo -- ipvsadm --clear
-echo "Reset succeed"
-echo "-------------"
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+. "$DIR/env.sh"
+
+echo "Copy scripts to all nodes"
+echo "-------------------------"
+parallel --tag -- $SCP --recurse "$DIR/resource" $USER@{}:/tmp ::: "$MASTER" $NODES
+
+echo "Reset all nodes"
+echo "---------------"
+parallel -vvv --tag -- "gcloud compute ssh {} -- sh /tmp/resource/reset.sh" ::: "$MASTER" $NODES
+
